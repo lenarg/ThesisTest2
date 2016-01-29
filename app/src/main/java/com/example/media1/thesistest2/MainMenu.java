c124e731d0d8df4e5c02f030ce67877631a9f929
lenarg <lenareg@hotmail.com>
2016-01-29 13:47:44 +0200

    After succesfully gitting

--- app/src/main/java/com/example/media1/thesistest2/MainMenu.java
+++ app/src/main/java/com/example/media1/thesistest2/MainMenu.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity  { //test comment
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
