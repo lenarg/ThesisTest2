432488db6f086bbd6e85008d172ce68baf601dfb
lenarg <lenareg@hotmail.com>
2016-01-29 13:53:13 +0200

    deleting some useless lines of code

--- app/src/main/java/com/example/media1/thesistest2/MainMenu.java
+++ app/src/main/java/com/example/media1/thesistest2/MainMenu.java
@@ -15,17 +15,7 @@ public class MainMenu extends AppCompatActivity  { //test comment
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
