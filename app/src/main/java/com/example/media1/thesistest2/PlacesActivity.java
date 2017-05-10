9f9d67ca459fbfd421ef7c6571b24082f5a480d0
lenarg <lenareg@hotmail.com>
2017-05-10 17:57:08 +0300

    tryiing with jason instead of xml 2

--- app/src/main/java/com/example/media1/thesistest2/PlacesActivity.java
+++ app/src/main/java/com/example/media1/thesistest2/PlacesActivity.java
@@ -65,21 +65,35 @@ import android.widget.TextView;
 import com.example.media1.thesistest2.JSONParser;
 
 public class PlacesActivity extends Activity {
-    TextView uid;
+   // TextView uid;
+   // TextView name1;
+   // TextView email1;
+    TextView place_id1;
+    TextView user_id1;
     TextView name1;
-    TextView email1;
+    TextView description1;
+    TextView type1;
+    TextView coordinates1;
     Button Btngetdata;
 
     //URL to get JSON Array
     private static String url = "http://zafora.icte.uowm.gr/~ictest00344/testjson.php";//"http://10.0.2.2/JSON/";
 
     //JSON Node Names
-    private static final String TAG_USER = "user";
-    private static final String TAG_ID = "id";
+    //private static final String TAG_USER = "user";
+    //private static final String TAG_ID = "id";
+    //private static final String TAG_NAME = "name";
+    //private static final String TAG_EMAIL = "email";
+    private static final String TAG_ALLPLACES = "allplaces";
+    private static final String TAG_PLACEID = "place_id";
+    private static final String TAG_USERID = "user_id";
     private static final String TAG_NAME = "name";
-    private static final String TAG_EMAIL = "email";
+    private static final String TAG_DESCRIPTION = "description";
+    private static final String TAG_TYPE = "type";
+    private static final String TAG_COORDINATES = "coordinates";
 
-    JSONArray user = null;
+    //JSONArray user = null;
+    JSONArray allplaces = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,9 +117,18 @@ public class PlacesActivity extends Activity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            uid = (TextView)findViewById(R.id.uid);
+            //uid = (TextView)findViewById(R.id.uid);
+            //name1 = (TextView)findViewById(R.id.name);
+            //email1 = (TextView)findViewById(R.id.email);
+            place_id1 = (TextView)findViewById(R.id.place_id);
+            user_id1 = (TextView)findViewById(R.id.user_id);
             name1 = (TextView)findViewById(R.id.name);
-            email1 = (TextView)findViewById(R.id.email);
+            description1 = (TextView)findViewById(R.id.description);
+            type1 = (TextView)findViewById(R.id.type);
+            coordinates1 = (TextView)findViewById(R.id.coordinates);
+
+
+
             pDialog = new ProgressDialog(PlacesActivity.this);
             pDialog.setMessage("Getting Data ...");
             pDialog.setIndeterminate(false);
@@ -127,18 +150,34 @@ public class PlacesActivity extends Activity {
             pDialog.dismiss();
             try {
                 // Getting JSON Array
-                user = json.getJSONArray(TAG_USER);
-                JSONObject c = user.getJSONObject(0);
+                //user = json.getJSONArray(TAG_USER);
+                allplaces = json.getJSONArray(TAG_ALLPLACES);
+                //JSONObject c = user.getJSONObject(0);
+                JSONObject c = allplaces.getJSONObject(0);
 
                 // Storing  JSON item in a Variable
-                String id = c.getString(TAG_ID);
+                //String id = c.getString(TAG_ID);
+                //String name = c.getString(TAG_NAME);
+                //String email = c.getString(TAG_EMAIL);
+                String places_id = c.getString(TAG_PLACEID);
+                String user_id = c.getString(TAG_USERID);
                 String name = c.getString(TAG_NAME);
-                String email = c.getString(TAG_EMAIL);
+                String description = c.getString(TAG_DESCRIPTION);
+                String type = c.getString(TAG_TYPE);
+                String coordinates = c.getString(TAG_COORDINATES);
 
                 //Set JSON Data in TextView
-                uid.setText(id);
+                //uid.setText(id);
+                //name1.setText(name);
+                //email1.setText(email);
+                place_id1.setText(places_id);
+                user_id1.setText(user_id);
                 name1.setText(name);
-                email1.setText(email);
+                description1.setText(description);
+                type1.setText(type);
+                coordinates1.setText(coordinates);
+
+
 
             } catch (JSONException e) {
                 e.printStackTrace();
