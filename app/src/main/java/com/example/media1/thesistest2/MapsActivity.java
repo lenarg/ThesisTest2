21500bb4aa60fed7fb611b594462be37e04ed035
lenarg <lenareg@hotmail.com>
2017-06-20 14:39:55 +0300

    markers showed up on map!!!

--- app/src/main/java/com/example/media1/thesistest2/MapsActivity.java
+++ app/src/main/java/com/example/media1/thesistest2/MapsActivity.java
@@ -4,9 +4,11 @@ import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.EditText;
@@ -17,8 +19,11 @@ import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -28,6 +33,10 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -36,9 +45,142 @@ import java.util.EventListener;
 import java.util.List;
 import java.util.Locale;
 
+//test from so
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static com.example.media1.thesistest2.R.id.map;
+
+/* //trying http://stackoverflow.com/questions/29724192/using-json-for-android-maps-api-markers-not-showing-up
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    //private static final String LOG_TAG = "ExampleApp";
+
+    //private static final String SERVICE_URL = "https://api.myjson.com/bins/4jb09";
+
+    protected GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        //setUpMapIfNeeded();
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (map == null) {
+            MapFragment mapFragment = (MapFragment) getFragmentManager()
+                    .findFragmentById(R.id.map);
+            map = mapFragment.getMap(); //mapFragment.getMap();
+            if (map != null) {
+                //setUpMap();
+                new MarkerTask().execute();
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        new MarkerTask().execute();
+
+    }
+
+    private class MarkerTask extends AsyncTask<Void, Void, String> { //added by so
+
+        private static final String LOG_TAG = "ExampleApp";
+
+        private static final String SERVICE_URL = "https://api.myjson.com/bins/4jb09";
+
+        // Invoked by execute() method of this object
+        @Override
+        protected String doInBackground(Void... args) {
+
+            HttpURLConnection conn = null;
+            final StringBuilder json = new StringBuilder();
+            try {
+                // Connect to the web service
+                URL url = new URL(SERVICE_URL);
+                conn = (HttpURLConnection) url.openConnection();
+                InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+                // Read the JSON data into the StringBuilder
+                int read;
+                char[] buff = new char[1024];
+                while ((read = in.read(buff)) != -1) {
+                    json.append(buff, 0, read);
+                }
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error connecting to service", e);
+                //throw new IOException("Error connecting to service", e); //uncaught
+            } finally {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            }
+
+            return json.toString();
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(String json) {
+
+            try {
+                // De-serialize the JSON string into an array of city objects
+                JSONArray jsonArray = new JSONArray(json);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObj = jsonArray.getJSONObject(i);
+
+                    LatLng latLng = new LatLng(jsonObj.getJSONArray("latlng").getDouble(0),
+                            jsonObj.getJSONArray("latlng").getDouble(1));
+
+                    //move CameraPosition on first result
+                    if (i == 0) {
+                        CameraPosition cameraPosition = new CameraPosition.Builder()
+                                .target(latLng).zoom(13).build();
+
+                        map.animateCamera(CameraUpdateFactory
+                                .newCameraPosition(cameraPosition));
+                    }
+
+                    // Create a marker for each city in the JSON data.
+                    map.addMarker(new MarkerOptions()
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                            .title(jsonObj.getString("name"))
+                            .snippet(Integer.toString(jsonObj.getInt("population")))
+                            .position(latLng));
+                }
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "Error processing JSON", e);
+            }
+
+        }
+    } //till here
+}
+*/
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback { //now
+
+    //private static final String LOG_TAG = "ExampleApp";
+
+    //private static final String SERVICE_URL = "http://zafora.icte.uowm.gr/~ictest00344/testjson.php";//"https://api.myjson.com/bins/4jb09";
 
     private GoogleMap mMap;
+    //private final static String URL = "xyzz"; // trying this http://stackoverflow.com/questions/22650959/how-to-add-and-display-multiple-markers-that-are-parsed-from-a-json-object?rq=1
+    //public static final String TAG = "MapsEarthquakeMapActivity";  //
 
     // Create a LatLngBounds that includes West Macedonia Perfecture.
     //private LatLngBounds AUSTRALIA = new LatLngBounds(new LatLng(39.821813, 20.784578), new LatLng(40.944606, 22.181563)); //center (40.383210, 21.483071)
@@ -59,12 +201,105 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
+        //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        //        .findFragmentById(R.id.map);
+        //mapFragment.getMapAsync(this);
+        setUpMapIfNeeded();
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mMap = mapFragment.getMap();//getMapAsync(this);//getMap();
+            if (mMap != null) {
+                //setUpMap();
+                new MarkerTask().execute();
+            }
+        }
+    }
+
+    //for marker showing up
+    private class MarkerTask extends AsyncTask<Void, Void, String> {
+
+        private static final String LOG_TAG = "ExampleApp";
+
+        private static final String SERVICE_URL = "https://api.myjson.com/bins/4jb09";
+
+        // Invoked by execute() method of this object
+        @Override
+        protected String doInBackground(Void... args) {
+
+            HttpURLConnection conn = null;
+            final StringBuilder json = new StringBuilder();
+            try {
+                // Connect to the web service
+                URL url = new URL(SERVICE_URL);
+                conn = (HttpURLConnection) url.openConnection();
+                InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+                // Read the JSON data into the StringBuilder
+                int read;
+                char[] buff = new char[1024];
+                while ((read = in.read(buff)) != -1) {
+                    json.append(buff, 0, read);
+                }
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error connecting to service", e);
+                //throw new IOException("Error connecting to service", e); //uncaught
+            } finally {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            }
+
+            return json.toString();
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(String json) {
+
+            try {
+                // De-serialize the JSON string into an array of city objects
+                JSONArray jsonArray = new JSONArray(json);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObj = jsonArray.getJSONObject(i);
+
+                    LatLng latLng = new LatLng(jsonObj.getJSONArray("latlng").getDouble(0),
+                            jsonObj.getJSONArray("latlng").getDouble(1));
+
+                    //move CameraPosition on first result
+                    if (i == 0) {
+                        CameraPosition cameraPosition = new CameraPosition.Builder()
+                                .target(latLng).zoom(13).build();
+
+                        mMap.animateCamera(CameraUpdateFactory
+                                .newCameraPosition(cameraPosition));
+                    }
+
+                    // Create a marker for each city in the JSON data.
+                    mMap.addMarker(new MarkerOptions()
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                            .title(jsonObj.getString("name"))
+                            .snippet(Integer.toString(jsonObj.getInt("population")))
+                            .position(latLng));
+                }
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "Error processing JSON", e);
+            }
+
+        }
+    }
+
+
 
     //When the Search button is pressed
     public void onSearch(View view){
@@ -228,7 +463,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }  */
 
 
-    }
+    } //now
 
     //otan dokimaza me to xml
     /** public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -292,6 +527,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     } **/
 
+    //dokimazontas me to json
+    //1st try
+    /**
+   @Override
+    protected void onPostExecute(String result) {
 
+        // Had to create this hard coded JSON result string to have something to test with.
+        result = "[ { " + "\"point\":\"40.259357,21.549875\"," +  "\"name5\":\"Siatista\"}]";
 
-}
+        try {
+            JSONArray json = new JSONArray(result);
+
+            for (int i = 0; i < json.length(); i++) {
+                JSONObject e = json.getJSONObject(i);
+                String point = e.getString("point");
+
+                String[] point2 = point.split(",");
+                double lat1 = Double.parseDouble(point2[0]);
+                double lng1 = Double.parseDouble(point2[1]);
+
+                mMap.addMarker(new MarkerOptions().title(e.getString("name5")).position(new LatLng(lat1, lng1)));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }**/
+
+}/**/ //now
